2b4ed9095e1840775e7fa33f43aa8ee75167b1d8
GabiAharon <146546637+GabiAharon@users.noreply.github.com>
2024-11-26 22:48:09 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// קובץ זה לא יעלה לגיטהאב
+const ACCESS_CONFIG = {
+    centers: {
+        'קרית מלאכי': {
+            encryptedLink: btoa('https://docs.google.com/spreadsheets/d/1N2i3erI_i8odTnl7f4Z2r0RYaYMITpCD363S8VRzVtw/edit?usp=sharing'),
+            password: '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92'  // shalev123
+        },
+        'כפר חבד': {
+            encryptedLink: btoa('https://docs.google.com/spreadsheets/d/163_7WH6g9Q8uoW-U5rJTwnlXZy7J7m0VrXmcXyV3AqY/edit?gid=0'),
+            password: '97ff33f1d58647bfa69e614ba77ba88f58d5f11d5a2d0c09f5f4f6995db98d94'  // menashe123
+        }
+    }
+}; 
