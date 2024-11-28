badb8a800c37a6e2a96ae38a26a36491db69bebd
GabiAharon <146546637+GabiAharon@users.noreply.github.com>
2024-11-28 14:34:10 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// קובץ זה לא יעלה לגיטהאב
+const ACCESS_CONFIG = {
+    centers: {
+        'kiryat_malachi': {
+            encryptedLink: btoa('https://docs.google.com/spreadsheets/d/1N2i3erI_i8odTnl7f4Z2r0RYaYMITpCD363S8VRzVtw/edit?usp=sharing'),
+            password: 'Shalev2024'
+        },
+        'kfar_chabad': {
+            encryptedLink: btoa('https://docs.google.com/spreadsheets/d/163_7WH6g9Q8uoW-U5rJTwnlXZy7J7m0VrXmcXyV3AqY/edit?gid=0'),
+            password: 'Menashe2024'
+        }
+    }
+}; 
